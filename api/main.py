a8fda6b8387d4e0e54541b2f80f11de216dee637
mpkumar1947 <manemonip23@iitk.ac.in>
2026-03-30 01:05:54 +0530

    feat: configure CORS for local dev and gradiator.tech

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from slowapi.errors import RateLimitExceeded
-
+from fastapi.middleware.cors import CORSMiddleware 
 # Local Imports
 # from .celery_app import app as celery_application
 from .utils.limiter import limiter, _rate_limit_exceeded_handler
@@ -16,7 +16,21 @@ app = FastAPI(
     description="API backend for fetching IITK course grade distributions.",
     version="0.1.0",
 )
-
+origins = [
+    "http://localhost:5173",       # For our local React development
+    "http://localhost:3000",       # Just in case to use port 3000
+    "http://gradiator.tech",       # our new production domain
+    "https://gradiator.tech",
+    "http://www.gradiator.tech",
+    "https://www.gradiator.tech",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 # Rate Limiter Setup
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
